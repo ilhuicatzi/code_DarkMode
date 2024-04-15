0a8df774eb58d2773e6bfe9c3293fd1e73ad53d3
Gonzalo Ilhuicatzi <g.ilhuicatzi@gmail.com>
2024-04-15 03:35:43 -0500

    navbar: useNavigation links gitHub

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,40 @@
 import { FaGithub } from "react-icons/fa";
 import ButtonDarkMode from "./ButtonDarkMode";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const navigate = useNavigate();
   return (
     <nav className="flex justify-end items-center px-8 py-2">
       <ul className="flex  items-center gap-2">
         <li>
-          <a href="/" className="px-3 py-0.5 hover:bg-zinc-200/60 hover:dark:bg-zinc-800 rounded-lg ">Home</a>
+          <button
+            onClick={() => navigate("/")}
+            className="px-3 py-0.5 hover:bg-zinc-200/60 hover:dark:bg-zinc-800 rounded-lg "
+          >
+            Home
+          </button>
         </li>
         <li>
-          <a href="/about"  className="px-3 py-0.5 hover:bg-zinc-200/60 hover:dark:bg-zinc-800 rounded-lg ">About</a>
+          <button
+            onClick={() => navigate("/about")
+            }
+            className="px-3 py-0.5 hover:bg-zinc-200/60 hover:dark:bg-zinc-800 rounded-lg "
+          >
+            About
+          </button>
         </li>
         <li>
-          <button className="hover:bg-zinc-200/60 hover:dark:bg-zinc-800 p-2 rounded-full mr-1">
-            <FaGithub className="w-5 h-5"  />
+          <button
+            onClick={() =>
+              window.open(
+                "https://github.com/ilhuicatzi/code_DarkMode",
+                "_blank"
+              )
+            }
+            className="hover:bg-zinc-200/60 hover:dark:bg-zinc-800 p-2 rounded-full mr-1"
+          >
+            <FaGithub className="w-5 h-5" />
           </button>
         </li>
         <li>
@@ -21,7 +42,7 @@ function Navbar() {
         </li>
       </ul>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
